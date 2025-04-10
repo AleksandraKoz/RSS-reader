01dabe70bcb0da93fe6d336b4961e1e057f67b84
Aleksandra Kozak <a.kozak@expansio.pl>
2025-04-10 18:23:27 +0200

    Run Prettier

--- store/store.js
+++ store/store.js
@@ -7,25 +7,22 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import newsReducer from './News';
 
 const persistConfig = {
-    key: 'root',
-    storage: AsyncStorage,
+  key: 'root',
+  storage: AsyncStorage,
 };
 
 const rootReducer = combineReducers({
-    news: newsReducer,
+  news: newsReducer,
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middlewares = [thunk];
 if (__DEV__) {
-    middlewares.push(logger);
+  middlewares.push(logger);
 }
 
-const store = createStore(
-    persistedReducer,
-    applyMiddleware(...middlewares)
-);
+const store = createStore(persistedReducer, applyMiddleware(...middlewares));
 
 const persistor = persistStore(store);
 
