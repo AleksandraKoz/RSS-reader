bd30cb4d2fa04e3b1ce331c76a41b0c8323553d7
Aleksandra Kozak <a.kozak@expansio.pl>
2025-04-12 01:52:47 +0200

    Add EmptyList component to display when there is no news fetched

---
+++ src/NewsList/EmptyList.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StyleSheet, Text } from 'react-native';
+import { connect } from 'react-redux';
+
+import Wrapper from '../components/Wrapper.tsx';
+
+interface IEmptyList {
+  feedUrl: string;
+}
+
+const EmptyList = ({ feedUrl }: IEmptyList): React.JSX.Element => {
+  return (
+    <Wrapper>
+      <Text style={styles.titleText}>No articles found in the "{feedUrl}" feed.</Text>
+    </Wrapper>
+  );
+};
+
+const mapStateToProps = (state: any) => ({});
+
+const mapDispatchToProps = (dispatch: any) => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(EmptyList);
+
+const styles = StyleSheet.create({
+  titleText: {
+    color: 'rgba(18, 52, 88,1)',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginVertical: 10,
+    textAlign: 'center',
+  },
+});
