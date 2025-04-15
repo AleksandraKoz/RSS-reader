931c6c5adbfb011d3a9fd44432b0447d3ff51a7a
Aleksandra Kozak <a.kozak@expansio.pl>
2025-04-15 17:55:30 +0200

    Add usage of extracted buttonVariant enum

--- src/components/Base/Button.tsx
+++ src/components/Base/Button.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { TouchableOpacity, Text, StyleSheet, ViewStyle, TextStyle } from 'react-native';
 
-type ButtonVariant = 'primary' | 'danger' | 'cancel' | 'cancel-outline' | 'danger-outline';
+import { ButtonVariant } from './ButtonVariants.ts';
 
 interface IButton {
   title: string;
@@ -10,55 +10,66 @@ interface IButton {
   style?: ViewStyle;
 }
 
-const Button = ({ title, onPress, variant = 'primary', style }: IButton): React.JSX.Element => {
+const Button = ({
+  title,
+  onPress,
+  variant = ButtonVariant.Primary,
+  style,
+}: IButton): React.JSX.Element => {
   return (
-    <TouchableOpacity onPress={onPress} style={[styles.buttonBase, styles[variant], style]}>
-      <Text style={textStyles[variant]}>{title}</Text>
+    <TouchableOpacity onPress={onPress} style={[styles.buttonBaseStyle, styles[variant], style]}>
+      <Text style={[textStyles.textBaseStyle, textStyles[variant]]}>{title}</Text>
     </TouchableOpacity>
   );
 };
 
 export default Button;
 
-const styles = StyleSheet.create<Record<ButtonVariant | 'buttonBase', ViewStyle>>({
-  buttonBase: {
-    borderRadius: 8,
-    paddingHorizontal: 20,
-    paddingVertical: 12,
+const styles = StyleSheet.create<Record<ButtonVariant | 'buttonBaseStyle', ViewStyle>>({
+  [ButtonVariant.Primary]: {
+    backgroundColor: '#007aff',
   },
-  cancel: {
+  [ButtonVariant.Danger]: {
+    backgroundColor: '#9d0000',
+  },
+  [ButtonVariant.Cancel]: {
     backgroundColor: '#888',
   },
-  'cancel-outline': {
+  [ButtonVariant.CancelOutline]: {
     backgroundColor: '#fff',
     borderColor: '#888',
     borderWidth: 1,
   },
-  danger: {
-    backgroundColor: '#9d0000',
-  },
-  'danger-outline': {
+  [ButtonVariant.DangerOutline]: {
     backgroundColor: '#fff',
     borderColor: '#9d0000',
     borderWidth: 1,
   },
-  primary: {
-    backgroundColor: '#007aff',
+  buttonBaseStyle: {
+    borderRadius: 8,
+    paddingHorizontal: 20,
+    paddingVertical: 12,
   },
 });
 
-const textStyles: Record<ButtonVariant, TextStyle> = {
-  primary: { color: '#fff', fontWeight: 'bold', textAlign: 'center' },
-  danger: { color: '#fff', fontWeight: 'bold', textAlign: 'center' },
-  cancel: { color: '#fff', fontWeight: 'bold', textAlign: 'center' },
-  'cancel-outline': {
-    color: '#888',
+const textStyles: Record<ButtonVariant | 'textBaseStyle', TextStyle> = {
+  textBaseStyle: {
     fontWeight: 'bold',
     textAlign: 'center',
   },
-  'danger-outline': {
+  [ButtonVariant.Primary]: {
+    color: '#fff',
+  },
+  [ButtonVariant.Danger]: {
+    color: '#fff',
+  },
+  [ButtonVariant.Cancel]: {
+    color: '#fff',
+  },
+  [ButtonVariant.CancelOutline]: {
+    color: '#888',
+  },
+  [ButtonVariant.DangerOutline]: {
     color: '#9d0000',
-    fontWeight: 'bold',
-    textAlign: 'center',
   },
 };
