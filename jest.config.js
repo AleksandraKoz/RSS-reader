fde4f7621a08efb28830bb38be0d676fa0a54204
Aleksandra Kozak <a.kozak@expansio.pl>
2025-04-16 11:38:38 +0200

    Add necessary configurations

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   preset: 'react-native',
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  transform: {
+    '^.+\\.tsx?$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|@react-navigation|@testing-library)/)',
+  ],
+  moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx'],
 };
